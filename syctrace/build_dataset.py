ee9ded4b23f7b42bb0fa9214b1afbc3523fdd403
Juan Manuel Ortiz Suarez <jmortiz@syc.com.co>
2021-05-25 13:58:04 -0500

    Data augmentation in dataset build.

--- syctrace/build_dataset.py
+++ syctrace/build_dataset.py
@@ -5,8 +5,10 @@ from pathlib import Path
 from lxml import etree
 from sklearn.model_selection import train_test_split
 from hashlib import sha1
+import random
+import numpy as np
 
-def process_annotation(xml_tree, source_path: str, size: int, classes: dict):
+def process_annotation(xml_tree, source_path: str, size: int, classes: dict, shift_range: float = None, brightness: int = None):
 
     img_path = Path(source_path) / xml_tree.find('folder').text / xml_tree.find('filename').text
 
@@ -33,6 +35,28 @@ def process_annotation(xml_tree, source_path: str, size: int, classes: dict):
     else:
         pass
 
+    # random shift
+    if shift_range is not None:
+            x_shift = random.randrange(int(-size * shift_range), int(size * shift_range), step=1)
+            y_shift = random.randrange(int(-size * shift_range), int(size * shift_range), step=1)
+            M = np.float32([
+                [1, 0, x_shift],
+                [0, 1, y_shift]
+            ])
+
+            img_res = cv.warpAffine(img_res, M, (img_res.shape[1], img_res.shape[0]), borderMode=cv.BORDER_CONSTANT, borderValue=(0, 0, 0))
+    # random brightness
+    if brightness is not None:
+        value = random.uniform(1-brightness, 1+brightness)
+        hsv = cv.cvtColor(img_res, cv.COLOR_BGR2HSV)
+        hsv = np.array(hsv, dtype=np.float64)
+        hsv[:,:,1] = hsv[:,:,1]*value
+        hsv[:,:,1][hsv[:,:,1]>255]  = 255
+        hsv[:,:,2] = hsv[:,:,2]*value
+        hsv[:,:,2][hsv[:,:,2]>255]  = 255
+        hsv = np.array(hsv, dtype = np.uint8)
+        img_res = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+
     yolo_labels = []
     for label in xml_tree.findall('object'):
         name = label.find('name').text
@@ -56,6 +80,25 @@ def process_annotation(xml_tree, source_path: str, size: int, classes: dict):
         y2_p = y2_r * (img_res.shape[0] - (top_p + bottom_p)) + top_p
         center = (x1_p + (x2_p - x1_p)/2, y1_p + (y2_p - y1_p)/2)
 
+        # update label points if random shift applied
+        if shift_range is not None:
+            x1_p += x_shift
+            y1_p += y_shift
+            x2_p += x_shift
+            y2_p += y_shift
+
+            x1_p = size if x1_p > size else x1_p
+            y1_p = size if y1_p > size else y1_p
+            x2_p = size if x2_p > size else x2_p
+            y2_p = size if y2_p > size else y2_p
+
+            x1_p = 0 if x1_p < 0 else x1_p
+            y1_p = 0 if y1_p < 0 else y1_p
+            x2_p = 0 if x2_p < 0 else x2_p
+            y2_p = 0 if y2_p < 0 else y2_p
+
+            center = (x1_p + (x2_p - x1_p)/2, y1_p + (y2_p - y1_p)/2)
+
         center_r = (center[0]/img_res.shape[1], center[1]/img_res.shape[0])
         width_r = (x2_p - x1_p)/img_res.shape[1]
         height_r = (y2_p -  y1_p)/img_res.shape[0]
@@ -69,7 +112,7 @@ def process_annotation(xml_tree, source_path: str, size: int, classes: dict):
 
         yolo_labels.append((idx, center_r[0], center_r[1], width_r, height_r))
 
-    #cv.imwrite('result_img.png', img_res)
+    # cv.imwrite('result_img.png', img_res)
     return img_res, xml_tree.find('filename').text, yolo_labels
 
 
@@ -79,6 +122,7 @@ if __name__ == '__main__':
     ap.add_argument('--source', required=True, type=str, help='source path')
     ap.add_argument('--size', required=True, type=int, help='')
     ap.add_argument('--test', required=True, type=float, help='proportion of test set')
+    ap.add_argument('--augment', required=True, type=int, help='Number of copies with random transformations for each image')
     ap.add_argument('--output', required=True, type=str, help='output folder')
 
     ARGS = ap.parse_args()
@@ -95,7 +139,7 @@ if __name__ == '__main__':
         annotations.append(root)
 
     # train test split
-    annotations_train, annotations_test = train_test_split(annotations, test_size=ARGS.test, stratify=classes, random_state=42)
+    annotations_train, annotations_test = train_test_split(annotations, test_size=ARGS.test, stratify=classes, random_state=811295)
 
     unique_classes = sorted(set(classes))
     unique_classes = {c:i for i, c in enumerate(unique_classes)}
@@ -105,16 +149,17 @@ if __name__ == '__main__':
     folder_train_labels = Path(ARGS.output) / 'train' / 'labels'
     folder_train_labels.mkdir(parents=True, exist_ok=True)
     for annotation in annotations_train:
-        img, img_name, labels = process_annotation(annotation, ARGS.source, size=ARGS.size, classes=unique_classes)
-        file_id = sha1(img).hexdigest()
-        file_image_name = f'{img_name}.{file_id}.png'
-        file_labels_name = f'{img_name}.{file_id}.txt'
+        for n in range(ARGS.augment):
+            img, img_name, labels = process_annotation(annotation, ARGS.source, size=ARGS.size, classes=unique_classes, shift_range=0.15, brightness=0.4)
+            file_id = sha1(img).hexdigest()
+            file_image_name = f'{img_name}.{file_id}.png'
+            file_labels_name = f'{img_name}.{file_id}.txt'
 
-        cv.imwrite(str(folder_train_img / file_image_name), img)
+            cv.imwrite(str(folder_train_img / file_image_name), img)
 
-        with open(folder_train_labels / file_labels_name, 'w') as f:
-            for l in labels:
-                f.write(f'{l[0]} {l[1]} {l[2]} {l[3]} {l[4]}\n')
+            with open(folder_train_labels / file_labels_name, 'w') as f:
+                for l in labels:
+                    f.write(f'{l[0]} {l[1]} {l[2]} {l[3]} {l[4]}\n')
 
     folder_test_img = Path(ARGS.output) / 'test' / 'images'
     folder_test_img.mkdir(parents=True, exist_ok=True)
@@ -131,12 +176,3 @@ if __name__ == '__main__':
         with open(folder_test_labels / file_labels_name, 'w') as f:
             for l in labels:
                 f.write(f'{l[0]} {l[1]} {l[2]} {l[3]} {l[4]}\n')
-
-
-
-
-
-
-
-
-
